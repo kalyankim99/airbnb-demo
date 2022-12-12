513867421e12236785a119e017f623c7d005fef2
unknown <kalyankim99@gmail.com>
2022-12-12 13:27:10 +0800

    day one build

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-52 py-14 bg-gray-100 text-gray-600">
+        <div className="grid items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10 px-80 py-14 bg-gray-100 text-gray-600">
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">    ABOUT   </h5>
                 <p> How airbnb works    </p>
